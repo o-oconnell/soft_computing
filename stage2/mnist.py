7abd5d22827416729f1f01078bb961dfdc485400
o-oconnell <reamonoshea@gmail.com>
2022-11-09 17:22:12 -0700

    increase learning rate

--- stage2/mnist.py
+++ stage2/mnist.py
@@ -135,7 +135,7 @@ def run():
 
     i = 0
     while i < len(x_train):
-        backpropagate(x_train[i].reshape((28*28, 1)), y_train[i], learning_rate=0.03)
+        backpropagate(x_train[i].reshape((28*28, 1)), y_train[i], learning_rate=0.4)
         i += 1
 
                 
