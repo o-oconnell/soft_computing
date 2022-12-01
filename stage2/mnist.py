903b16eec0f6f5847415d6bf9fc9856aaac0c6e8
o-oconnell <reamonoshea@gmail.com>
2022-12-01 16:54:44 -0700

    reproducible network 1 implementation

--- stage2/mnist.py
+++ stage2/mnist.py
@@ -5,25 +5,16 @@ import time
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 (x_train, x_test) = (x_train / 255.0, x_test / 255.0)
 
-dimensions = [28*28, 30, 10]
+dimensions = [28*28, 100, 10]
 biases = []
 weights = []
 
-# Step 0
-
+numpy.random.seed(0)
 for i in range(1, len(dimensions)):
     weights.append(2 * numpy.random.random_sample((dimensions[i], dimensions[i-1])) - 1)
         
 for i in range(1, len(dimensions)):
     biases.append(2 * numpy.random.random_sample((dimensions[i], 1)) - 1)
-
-
-# See p.324 of Fausett's artificial neural systems, we are using a quadratic cost.
-def quadratic_E(target, found):
-    return 0.5 * (target - found)**2
-
-def E_prime(target, found):
-    return (target - found)
 
 def binary_sigmoid(x):
     return 1/(1.0 + numpy.exp(-x))
@@ -37,8 +28,6 @@ def binary_sigmoid_derivative(x):
 # to the biases/weights)
 
 def check_correctness(inp, desired):
-    # print("input shape is "+str(inp.shape))
-    
     current_activation = inp
     all_activations, f_in_tot = [], []
     all_activations.append(inp)
@@ -54,19 +43,9 @@ def check_correctness(inp, desired):
             max_idx = i
             max_v = current_activation[i]
             
-    # print("desired result " + str(desired))
-    # print("found result " + str(max_idx))
-    # print("current activation:")
-    # print(current_activation)
     if (desired == max_idx):
-        # print("max idx " + str(max_idx))
-        # print("desired " + str(desired))
-        # print("activations: " + str(current_activation))
         return 1
     else:
-        # print("max idx " + str(max_idx))
-        # print("desired " + str(desired))
-        # print("activations:"+str(current_activation))
         return 0
         
 def backpropagate(inp, desired, learning_rate):
@@ -85,20 +64,50 @@ def backpropagate(inp, desired, learning_rate):
         current_activation = binary_sigmoid(f_in)
         all_activations.append(current_activation)
 
-        
     # Backpropagation of error
     # Step 6-8: backpropagation of error and updating of weights/biases
 
     # Array to store all errors as we move backward through the network
     s = []
     # error information term
+    
+    # For the MSE cost, we have
+    # cost = 1/2 * (desired - actual)^2
+    # actual = sigmoid(input) = sigmoid(weights * activations_of_previous_layer + biases)
+    
+    # dCost / dWeight = 2 * (1/2) * (desired - actual) * -sigmoid_prime(input) * activations_of_previous_layer
+    # = (actual - desired) * sigmoid_prime(input) * activations_of_previous_layer
+
+    # MSE COST:
+    # ---------------------
     sk = (all_activations[len(all_activations)-1] - desired) * binary_sigmoid_derivative(f_in_tot[len(f_in_tot)-1])
     s.append(sk)
-    
-    # weight correction term
     delta_w = numpy.matmul(sk,
                         numpy.transpose(
                         all_activations[len(all_activations)-2]))
+    # ----------------------
+
+    # For the cross-entropy cost, we have:
+    # cost = -(desired * ln(actual) + (1 - desired) * ln(1 - actual))
+    # = -(yln(a)+(1-y)ln(1-a))
+    # dC/dW = -y/a*sp(wa_p+b)*a_p - (1-y)/(1-a)*-sp(wa_p+b)a_p, where a = sigmoid(w*a_p+b), and sp = derivative of sigmoid, and a_p is the activations of the previous layer
+    # = sp(wa_p+b)a_p * (-y(1-a)/(a(1-a)) + a(1-y)/(a(1-a)))
+    # = sp(wa_p+b)a_p * (-y(1-a) + a(1-y))/(a(1-a))
+    # = sp(wa_p+b)a_p * (-y + ya + a -ya)/(a(1-a))
+    # = sp(wa_p+b)a_p * (a - y)/(a(1-a))
+
+    # Since the sigmoid function has the property that sigmoid_prime(x) = sigmoid(x)(1-sigmoid(x)), we get:
+    # = a_p * (a - y)
+    # (similarly to the quadratic, dC/db = (a-y) = sk below)
+
+    # CROSS-ENTROPY COST
+    # -----------------
+    # sk = (all_activations[len(all_activations)-1] - desired) 
+    # s.append(sk)
+    # delta_w = numpy.matmul(sk,
+    #                     numpy.transpose(
+    #                     all_activations[len(all_activations)-2]))
+    # -----------------
 
     weight_changes = []
     bias_changes = []
@@ -138,15 +147,15 @@ def backpropagate(inp, desired, learning_rate):
         biases[i] -= learning_rate * bias_changes[i]
 
 def run():
-
     # Issues with our algorithm:
-    # 1. We update the weights as we go instead of waiting until all of the errors have been calculated. This is a problem because we need the original weights in order to backpropagate the error.
+    # 1. We updated the weights as we go instead of waiting until all of the errors have been calculated. This is a problem because we need the original weights in order to backpropagate the error.
     # For a hidden layer, the error S_in_j = sum over k of Sk * Wjk, so if we've changed Wjk prior to calculating S_in_j, we're not backpropagating the error according to the proof.
 
     # 2. Our y_train and y_test vectors were encoded as single integers. We needed an array of 10 values with a single one in order to execute the algorithm properly.
 
     # 3. Kind of an issue (still have not fixed): we are initializing the weights to small random values from a uniform distribution. It would be preferable to initialize them from a standard normal distribution as values would be clustered around zero and less likely to saturate.
 
+    # Reshape our images to suit a 784-neuron input layer
     x_train_0 = []
     x_test_0 = []
     for i in range(0, len(x_train)):
@@ -155,6 +164,8 @@ def run():
     for i in range(0, len(x_test)):
         x_test_0.append(x_test[i].reshape((28*28, 1)))
 
+
+    # Convert our labels to one-hot encoding
     y_train_0 = []
     y_test_0 = []
     for i in range(0, len(y_train)):
@@ -175,7 +186,7 @@ def run():
     for k in range(0, len(x_test)):
         n_correct += check_correctness(x_test[k].reshape((28*28, 1)), y_test[k])
 
-    print("Number of correct results after " + str(i) + " values have been fed through: ")
+    print("Number of correctly classified test images after training with " + str(i) + " images:")
     print(n_correct)
     print("out of")
     print(len(x_test))
